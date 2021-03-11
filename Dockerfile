09e8212e1e5ecabf1f93969f4c907aeb9d4c4003
SURYADEEP GUHA <suryag9401@gmail.com>
2021-03-10 17:37:20 -0800

    Addng new docker config and changes to app.py

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:3.7-alpine
+WORKDIR /app
+
+COPY requirements.txt requirements.txt
+
+RUN pip install -r requirements.txt
+EXPOSE 5000
+COPY . /app
+CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-  return 'Hello World!'
+  return 'Hello World! - Modified - Good start to docker :-)'
 
 
 if __name__ == '__main__':
